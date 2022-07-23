ca169dd1c59c990665b9c9844ad69aca069deac3
Jared Barnak <jared.j.barnak@gmail.com>
2022-07-22 20:03:09 -0400

    Working Set Up

---
+++ script/set_up_interfaces.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This scripts sets up the user space interface to send and receive
+# TCP segments on
+
+# Set up interface for receiving and sending
+# Interface name: tcp0
+sudo ip tuntap add dev tcp0 mod tun
+# Assign tcp0 the ip "10.0.0.150"
+sudo ip addr add 10.1.0.150/24 dev tcp0
+# Make it so that tpc0 is up
+sudo ip link set up dev tcp0
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <IPv4Layer.h>
+#include <Packet.h>
+#include <PcapFileDevice.h>
+
+int main() {
+	pcpp::RawPacket rawPacket;
+
+}
