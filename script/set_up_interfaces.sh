42758a356a7c6e908005f6d941d1583001d568e2
Jared Barnak <jared.j.barnak@gmail.com>
2022-07-22 20:44:15 -0400

    Set up PcapPlusPlus

--- script/set_up_interfaces.sh
+++ script/set_up_interfaces.sh
@@ -6,6 +6,6 @@
 # Interface name: tcp0
 sudo ip tuntap add dev tcp0 mod tun
 # Assign tcp0 the ip "10.0.0.150"
-sudo ip addr add 10.1.0.150/24 dev tcp0
+sudo ip addr add 10.0.0.150/24 dev tcp0
 # Make it so that tpc0 is up
 sudo ip link set up dev tcp0
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,25 @@
 #include <IPv4Layer.h>
 #include <Packet.h>
 #include <PcapFileDevice.h>
+#include "stdlib.h"
+#include "PcapLiveDeviceList.h"
+#include "SystemUtils.h"
 
 int main() {
-	pcpp::RawPacket rawPacket;
+	// We are capturing on the interface with the specified IP
+	std::string ipv4_address { "10.0.0.150" };
 
+	// Find the interface itself
+	pcpp::PcapLiveDevice* dev = pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDeviceByIp(ipv4_address);
+	if (dev == NULL) {
+		std::cerr << "Cannot find interface with the IPv4 of " << ipv4_address << "\n";
+		return 1;
+	}
+
+	// Esnures the device is open
+	if (!dev->open()) {
+		std::cerr << "Cannot open device\n";
+		std::cerr << "Be sure to run the binary with sudo\n";
+		return 1;
+	}
 }
