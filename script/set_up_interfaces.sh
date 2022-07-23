692f1ad405dc0acf415b707f46849664c3e404c7
Jared Barnak <jared.j.barnak@gmail.com>
2022-07-23 00:26:05 -0400

    Updated script so that tun interface receives the packet

--- script/set_up_interfaces.sh
+++ script/set_up_interfaces.sh
@@ -9,3 +9,12 @@ sudo ip tuntap add dev tcp0 mod tun
 sudo ip addr add 10.0.0.150/24 dev tcp0
 # Make it so that tpc0 is up
 sudo ip link set up dev tcp0
+
+# This is so that the packets actually get forwarded to the
+# tun interface.
+ip route add 10.0.0.150 dev tcp0 table 1234
+ip rule add from 10.0.0.150 lookup 1234 pref 10
+ip rule add from all lookup local pref 20
+ip rule del from all lookup local pref 0
+
+
